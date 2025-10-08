6799cc4a96d984fe88baf76e222698d1bfad0126
SatwantKaurNagi <80119939+SatwantKaurNagi@users.noreply.github.com>
2025-10-08 20:29:08 +0530

    3rd commit

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 print("This is my python code")
 
-""" This is my first python code"""
+""" This is my first Python code """
 
 print("I am learning python")
 
-""" This is my second python code"""
+""" This is my second Python code """
 
